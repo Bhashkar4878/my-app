f05964c3f7fe3cc0c33d9fdc6127d0382bd1fcce
Bhashkar Kumar <84316632+Bhashkar4878@users.noreply.github.com>
2025-11-19 07:44:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link, Navigate, useLocation } from 'react-router-dom';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Feed from './components/Feed';
